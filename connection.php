e0fef758d14b3656dd0031655f3a2133e800cc73
nagamallikarjuna <75171125+nagamallikarjuna@users.noreply.github.com>
2020-11-28 06:23:27 -0800

    uploaded my php project

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("shareddb-o.hosting.stackcp.net", "secretdi-31303909eb", "(N)(M)(N)%", "secretdi-31303909eb");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
